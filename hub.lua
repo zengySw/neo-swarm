91c256b5a25978c026ded1c634d72f68356df38f
zengy <o.borvenok@gmail.com>
2026-01-19 00:20:42 +0200

    xd

--- hub.lua
+++ hub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zengySw/neo-swarm/main/gui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zengySw/neo-swarm/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zengySw/neo-swarm/main/gui.lua"))()
